5aac57e9543649009eb83dfd3e520efecfd3896e
OriginalMidas <clayton.brez@gmail.com>
2016-02-10 00:12:46 -0600

    Completed assignment

--- src/lab/pkg2/Lab2.java
+++ src/lab/pkg2/Lab2.java
@@ -14,8 +14,12 @@ public class Lab2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
-    }
-    
+        TankFactory factory = new TankFactory();
+        UI ui = new UI();
+        ui.mainArea(factory);        
+    }    
 }
+
